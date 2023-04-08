280e7a6259829a05bb933cf5b4ab85f796981e2c
IkegamiXYZ <syukatu97019a@gmail.com>
2023-04-08 21:11:39 +0900

    5章修正

---
+++ next-blog/components/layout.js
@@ -0,0 +1,16 @@
+//layout.js
+
+import Header from "./header"
+import Footer from "./footer"
+
+const Layout = (props) => {
+  return (
+    <>
+      <Header />
+      <main>{props.children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
